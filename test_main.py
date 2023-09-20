e1d920fb4d169dec120353b9b09e319ed9889c87
Younes Kebour <ykebour@y.kebour-Legion-5-15IAH7>
2023-09-20 21:18:30 +0100

    adding census.csv data and creating main.py and test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,7 @@
-# test_my_module.py
-def dummy_test():
-    assert 1 + 1 == 2
+# test_main.py
+
+from main import add_numbers
+
+def test_addition():
+    result = add_numbers(1, 1)
+    assert result == 2
